415126322a829c6d2a4c8a2b2ede1167fd22b31d
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-13 15:15:42 +0300

    chore(widgets): pin bootstrap fallback to mapping runtime

--- ujg-timesheet.bootstrap.js
+++ ujg-timesheet.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgTimesheet", [], function() {
   var widgetCssFile = "ujg-timesheet.css";
   var widgetRuntimeFile = "ujg-timesheet.runtime.js";
   var runtimeAmd = "_ujgTimesheetRuntime";
-  var releaseRef = "1073461";
+  var releaseRef = "6804bf2";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
