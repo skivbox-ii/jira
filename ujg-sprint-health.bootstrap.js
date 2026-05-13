fd97a6eaf335fa7831a90fddc00cff3f10593da2
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-13 11:55:22 +0300

    chore(widgets): pin bootstrap to editable excel importer

--- ujg-sprint-health.bootstrap.js
+++ ujg-sprint-health.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgSprintHealth", [], function() {
   var widgetCssFile = "ujg-sprint-health.css";
   var widgetRuntimeFile = "ujg-sprint-health.runtime.js";
   var runtimeAmd = "_ujgSprintHealthRuntime";
-  var releaseRef = "7ba6103";
+  var releaseRef = "52d3601";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
--- ujg-user-activity.bootstrap.js
+++ ujg-user-activity.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgUserActivity", [], function() {
   var widgetCssFile = "ujg-user-activity.css";
   var widgetRuntimeFile = "ujg-user-activity.runtime.js";
   var runtimeAmd = "_ujgUserActivityRuntime";
-  var releaseRef = "7ba6103";
+  var releaseRef = "52d3601";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
