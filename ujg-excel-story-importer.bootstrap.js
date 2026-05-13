d279eaa21be7778b8de4ffabd7c04df657aa4159
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-13 11:56:23 +0300

    chore(widgets): pin bootstrap fallback to editable importer runtime

--- ujg-excel-story-importer.bootstrap.js
+++ ujg-excel-story-importer.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgExcelStoryImporter", [], function() {
   var widgetCssFile = "ujg-excel-story-importer.css";
   var widgetRuntimeFile = "ujg-excel-story-importer.runtime.js";
   var runtimeAmd = "_ujgExcelStoryImporterRuntime";
-  var releaseRef = "52d3601";
+  var releaseRef = "fd97a6e";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
