5f71c4d1f4c0c4197d0d78f9435ea5cc8d009cc7
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-13 12:52:10 +0300

    chore(widgets): pin bootstrap fallback to epic field runtime

--- ujg-project-analytics.bootstrap.js
+++ ujg-project-analytics.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgProjectAnalytics", [], function() {
   var widgetCssFile = "ujg-project-analytics.css";
   var widgetRuntimeFile = "ujg-project-analytics.runtime.js";
   var runtimeAmd = "_ujgProjectAnalyticsRuntime";
-  var releaseRef = "cdab09a";
+  var releaseRef = "646b469";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
