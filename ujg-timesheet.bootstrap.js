fe45bd1d656a12c174a5c2929c4682efbb95218f
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-14 13:16:52 +0300

    chore(widgets): pin bootstrap to excel sync fix

--- ujg-timesheet.bootstrap.js
+++ ujg-timesheet.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgTimesheet", [], function() {
   var widgetCssFile = "ujg-timesheet.css";
   var widgetRuntimeFile = "ujg-timesheet.runtime.js";
   var runtimeAmd = "_ujgTimesheetRuntime";
-  var releaseRef = "1c61cb1";
+  var releaseRef = "758782a";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
