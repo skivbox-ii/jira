41bd703d218eb61cf1e2598906eab5322dcbeac8
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-14 19:29:07 +0300

    chore(widgets): pin bootstrap to raw epic key fix

--- ujg-excel-story-importer.bootstrap.js
+++ ujg-excel-story-importer.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgExcelStoryImporter", [], function() {
   var widgetCssFile = "ujg-excel-story-importer.css";
   var widgetRuntimeFile = "ujg-excel-story-importer.runtime.js";
   var runtimeAmd = "_ujgExcelStoryImporterRuntime";
-  var releaseRef = "d13bfd1";
+  var releaseRef = "e0d4e54";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
