e2f447ba8a2e8b68453727a09c553d17399d0eeb
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-14 10:55:22 +0300

    chore(widgets): pin bootstrap fallback to toolbar icon fix

--- ujg-excel-story-importer.bootstrap.js
+++ ujg-excel-story-importer.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgExcelStoryImporter", [], function() {
   var widgetCssFile = "ujg-excel-story-importer.css";
   var widgetRuntimeFile = "ujg-excel-story-importer.runtime.js";
   var runtimeAmd = "_ujgExcelStoryImporterRuntime";
-  var releaseRef = "2de9f15";
+  var releaseRef = "dd14f2f";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
