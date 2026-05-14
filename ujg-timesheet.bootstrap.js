c6024268075af65b78489c190a5fdfe79e88daca
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-14 09:31:44 +0300

    chore(widgets): pin bootstrap fallback to mapping layout fix

--- ujg-timesheet.bootstrap.js
+++ ujg-timesheet.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgTimesheet", [], function() {
   var widgetCssFile = "ujg-timesheet.css";
   var widgetRuntimeFile = "ujg-timesheet.runtime.js";
   var runtimeAmd = "_ujgTimesheetRuntime";
-  var releaseRef = "4b812f0";
+  var releaseRef = "c2e8508";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
