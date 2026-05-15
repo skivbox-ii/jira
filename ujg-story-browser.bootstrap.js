68dd3dc27e0b9abc5157d79d17254884d214445a
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-15 15:41:32 +0300

    bootstrap: pin accepted blocker fix

--- ujg-story-browser.bootstrap.js
+++ ujg-story-browser.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgStoryBrowser", [], function() {
   var widgetCssFile = "ujg-story-browser.css";
   var widgetRuntimeFile = "ujg-story-browser.runtime.js";
   var runtimeAmd = "_ujgStoryBrowserRuntime";
-  var releaseRef = "9849cfe";
+  var releaseRef = "a0a0b80";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
