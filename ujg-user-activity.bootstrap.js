037b69947f2cae990accacad86f66238a9038f65
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-15 12:11:07 +0300

    bootstrap: pin child link direction fix

--- ujg-user-activity.bootstrap.js
+++ ujg-user-activity.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgUserActivity", [], function() {
   var widgetCssFile = "ujg-user-activity.css";
   var widgetRuntimeFile = "ujg-user-activity.runtime.js";
   var runtimeAmd = "_ujgUserActivityRuntime";
-  var releaseRef = "3f75008";
+  var releaseRef = "33ba30f";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
