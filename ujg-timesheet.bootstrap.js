1916533dbbe3657d38772850cd03573e5f19c5bf
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-16 19:39:42 +0300

    bootstrap: pin visited status color fix

--- ujg-timesheet.bootstrap.js
+++ ujg-timesheet.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgTimesheet", [], function() {
   var widgetCssFile = "ujg-timesheet.css";
   var widgetRuntimeFile = "ujg-timesheet.runtime.js";
   var runtimeAmd = "_ujgTimesheetRuntime";
-  var releaseRef = "be7c4e9";
+  var releaseRef = "8ddd85b";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
