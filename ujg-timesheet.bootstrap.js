4cb24ba1cf3d189dc9d8f37c837da7fa14aaadd3
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-20 19:34:24 +0300

    Pin bootstraps to trailing quote fix

--- ujg-timesheet.bootstrap.js
+++ ujg-timesheet.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgTimesheet", [], function() {
   var widgetCssFile = "ujg-timesheet.css";
   var widgetRuntimeFile = "ujg-timesheet.runtime.js";
   var runtimeAmd = "_ujgTimesheetRuntime";
-  var releaseRef = "e5f66dc";
+  var releaseRef = "1e96f19";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
