bb8266da2acaf948c51ce7559aa19df6610121bc
Alexsandr Zheleschikov <skivbox@skivbox.ru>
2026-05-20 19:55:17 +0300

    Pin bootstraps to description editor build

--- ujg-excel-story-importer.bootstrap.js
+++ ujg-excel-story-importer.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgExcelStoryImporter", [], function() {
   var widgetCssFile = "ujg-excel-story-importer.css";
   var widgetRuntimeFile = "ujg-excel-story-importer.runtime.js";
   var runtimeAmd = "_ujgExcelStoryImporterRuntime";
-  var releaseRef = "1e96f19";
+  var releaseRef = "434c4ea";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
--- ujg-project-analytics.bootstrap.js
+++ ujg-project-analytics.bootstrap.js
@@ -4,7 +4,7 @@ define("_ujgProjectAnalytics", [], function() {
   var widgetCssFile = "ujg-project-analytics.css";
   var widgetRuntimeFile = "ujg-project-analytics.runtime.js";
   var runtimeAmd = "_ujgProjectAnalyticsRuntime";
-  var releaseRef = "1e96f19";
+  var releaseRef = "434c4ea";
   var w = typeof window !== "undefined" && window ? window : (typeof globalThis !== "undefined" ? globalThis : {});
   w.__UJG_BOOTSTRAP__ = w.__UJG_BOOTSTRAP__ || { scriptPromises: {}, stylePromises: {} };
   var cache = w.__UJG_BOOTSTRAP__;
